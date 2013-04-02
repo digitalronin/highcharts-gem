de919efb2058d50e3385a8541f2ef9a416afd4e0
David Salgado <david@digitalronin.com>
2013-04-02 11:07:42 +0100

    require bar_chart.rb

--- lib/highcharts-gem.rb
+++ lib/highcharts-gem.rb
@@ -7,3 +7,4 @@ require 'json'
 require File.join(libdir, 'hash_with_indifferent_content')
 require File.join(libdir, 'chart')
 require File.join(libdir, 'line_chart')
+require File.join(libdir, 'bar_chart')
